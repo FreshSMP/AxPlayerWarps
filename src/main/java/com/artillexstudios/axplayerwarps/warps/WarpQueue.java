3d231adabb86bc53acc9d68c39e14e09d69d4435
R00tB33rMan <rootb33rman@gmail.com>
2025-03-31 00:06:05 -0400

    Utilize a ConcurrentHashMap instead of a WeakHashMap due to rare Folia exception

--- src/main/java/com/artillexstudios/axplayerwarps/warps/WarpQueue.java
+++ src/main/java/com/artillexstudios/axplayerwarps/warps/WarpQueue.java
@@ -4,9 +4,8 @@ import com.artillexstudios.axapi.utils.Cooldown;
 import com.artillexstudios.axapi.utils.Pair;
 import org.bukkit.entity.Player;
 
-import java.util.Iterator;
 import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +14,7 @@ import static com.artillexstudios.axplayerwarps.AxPlayerWarps.CONFIG;
 import static com.artillexstudios.axplayerwarps.AxPlayerWarps.MESSAGEUTILS;
 
 public class WarpQueue {
-    private static final WeakHashMap<Player, Pair<Warp, Long>> queue = new WeakHashMap<>();
+    private static final Map<Player, Pair<Warp, Long>> queue = new ConcurrentHashMap<>();
     private static final Cooldown<Player> bc = new Cooldown<>();
     private static ScheduledExecutorService service = null;
 
@@ -26,16 +25,21 @@ public class WarpQueue {
         service.scheduleAtFixedRate(() -> {
             long time = CONFIG.getLong("teleport-delay-seconds");
             try {
-                for (Iterator<Map.Entry<Player, Pair<Warp, Long>>> it = queue.entrySet().iterator(); it.hasNext(); ) {
-                    Map.Entry<Player, Pair<Warp, Long>> e = it.next();
-                    if (bc.hasCooldown(e.getKey())) continue;
-                    bc.addCooldown(e.getKey(), 1_000L);
-                    long spent = (System.currentTimeMillis() - e.getValue().getValue()) / 1_000L;
+                Map<Player, Pair<Warp, Long>> snapshot = Map.copyOf(queue);
+
+                for (Map.Entry<Player, Pair<Warp, Long>> entry : snapshot.entrySet()) {
+                    Player player = entry.getKey();
+                    Pair<Warp, Long> warpPair = entry.getValue();
+
+                    if (bc.hasCooldown(player)) continue;
+                    bc.addCooldown(player, 1_000L);
+
+                    long spent = (System.currentTimeMillis() - warpPair.getValue()) / 1_000L;
                     if (spent >= time) {
-                        e.getValue().getKey().completeTeleportPlayer(e.getKey());
-                        it.remove();
+                        warpPair.getKey().completeTeleportPlayer(player);
+                        queue.remove(player);
                     } else {
-                        MESSAGEUTILS.sendLang(e.getKey(), "teleport.in", Map.of("%seconds%", "" + (time - spent)));
+                        MESSAGEUTILS.sendLang(player, "teleport.in", Map.of("%seconds%", "" + (time - spent)));
                     }
                 }
             } catch (Exception ex) {
@@ -48,7 +52,7 @@ public class WarpQueue {
         queue.put(player, new Pair<>(warp, System.currentTimeMillis()));
     }
 
-    public static WeakHashMap<Player, Pair<Warp, Long>> getQueue() {
+    public static Map<Player, Pair<Warp, Long>> getQueue() {
         return queue;
     }
 
