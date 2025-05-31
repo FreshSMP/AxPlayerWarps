98ca270de4452a73a1125e0475d3321f08e4011c
R00tB33rMan <rootb33rman@gmail.com>
2025-05-31 13:28:45 -0400

    Utilized synchronizedMap and synchronize warp queue

--- src/main/java/com/artillexstudios/axplayerwarps/warps/WarpQueue.java
+++ src/main/java/com/artillexstudios/axplayerwarps/warps/WarpQueue.java
@@ -5,6 +5,7 @@ import com.artillexstudios.axapi.utils.Cooldown;
 import com.artillexstudios.axapi.utils.Pair;
 import org.bukkit.entity.Player;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.WeakHashMap;
@@ -13,26 +14,28 @@ import static com.artillexstudios.axplayerwarps.AxPlayerWarps.CONFIG;
 import static com.artillexstudios.axplayerwarps.AxPlayerWarps.MESSAGEUTILS;
 
 public class WarpQueue {
-    private static final Map<Player, Pair<Warp, Long>> queue = new WeakHashMap<>();
+    private static final Map<Player, Pair<Warp, Long>> queue = Collections.synchronizedMap(new WeakHashMap<>());
     private static final Cooldown<Player> cooldown = new Cooldown<>();
 
     public static void start() {
         Scheduler.get().runTimer(() -> {
             long time = CONFIG.getLong("teleport-delay-seconds");
             try {
-                for (Iterator<Map.Entry<Player, Pair<Warp, Long>>> it = queue.entrySet().iterator(); it.hasNext(); ) {
-                    Map.Entry<Player, Pair<Warp, Long>> e = it.next();
-                    if (cooldown.hasCooldown(e.getKey())) continue;
+                synchronized (queue) {
+                    for (Iterator<Map.Entry<Player, Pair<Warp, Long>>> it = queue.entrySet().iterator(); it.hasNext(); ) {
+                        Map.Entry<Player, Pair<Warp, Long>> e = it.next();
+                        if (cooldown.hasCooldown(e.getKey())) continue;
 
-                    cooldown.addCooldown(e.getKey(), 1_000L);
-                    long spent = (System.currentTimeMillis() - e.getValue().getValue()) / 1_000L;
-                    if (spent >= time) {
-                        e.getValue().getKey().completeTeleportPlayer(e.getKey());
-                        it.remove();
-                        continue;
+                        cooldown.addCooldown(e.getKey(), 1_000L);
+                        long spent = (System.currentTimeMillis() - e.getValue().getValue()) / 1_000L;
+                        if (spent >= time) {
+                            e.getValue().getKey().completeTeleportPlayer(e.getKey());
+                            it.remove();
+                            continue;
+                        }
+
+                        MESSAGEUTILS.sendLang(e.getKey(), "teleport.in", Map.of("%seconds%", "" + (time - spent)));
                     }
-
-                    MESSAGEUTILS.sendLang(e.getKey(), "teleport.in", Map.of("%seconds%", "" + (time - spent)));
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
