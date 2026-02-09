ea60ec2302d4a20cfddabe2df554b029e5b7c7bd
r00tb33rman <rootb33rman@gmail.com>
2026-02-09 14:50:44 -0500

    Rebase

--- src/main/java/com/artillexstudios/axplayerwarps/warps/WarpQueue.java
+++ src/main/java/com/artillexstudios/axplayerwarps/warps/WarpQueue.java
@@ -2,17 +2,17 @@ package com.artillexstudios.axplayerwarps.warps;
 
 import com.artillexstudios.axapi.scheduler.Scheduler;
 import com.artillexstudios.axapi.utils.Cooldown;
-import com.artillexstudios.axapi.utils.Pair;
 import org.bukkit.entity.Player;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static com.artillexstudios.axplayerwarps.AxPlayerWarps.CONFIG;
 import static com.artillexstudios.axplayerwarps.AxPlayerWarps.MESSAGEUTILS;
 
 public class WarpQueue {
-    private static final ConcurrentHashMap<Player, Pair<Warp, Long>> queue = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Player, TeleportData> queue = new ConcurrentHashMap<>();
     private static final Cooldown<Player> cooldown = Cooldown.create();
 
     public static void start() {
@@ -20,27 +20,35 @@ public class WarpQueue {
             long time = CONFIG.getLong("teleport-delay-seconds");
             long currentTime = System.currentTimeMillis();
 
-            queue.forEach((player, warpPair) -> {
+            queue.forEach((player, teleportData) -> {
                 if (cooldown.hasCooldown(player)) return;
                 cooldown.addCooldown(player, 1_000L);
-                long spent = (currentTime - warpPair.getValue()) / 1_000L;
+
+                Warp warp = teleportData.warp();
+                long spent = (currentTime - teleportData.date()) / 1_000L;
+
                 if (spent >= time) {
-                    if (queue.remove(player, warpPair)) {
-                        warpPair.getKey().completeTeleportPlayer(player);
+                    if (queue.remove(player, teleportData)) {
+                        if (!warp.getLocation().equals(teleportData.location())
+                                || warp.getTeleportPrice() != teleportData.teleportPrice()
+                                || !Objects.equals(warp.getCurrency(), teleportData.currency())) {
+                            MESSAGEUTILS.sendLang(player, "errors.warp-changed");
+                        } else {
+                            warp.completeTeleportPlayer(player);
+                        }
                     }
                 } else {
-                    MESSAGEUTILS.sendLang(player, "teleport.in", 
-                        Map.of("%seconds%", String.valueOf(time - spent)));
+                    MESSAGEUTILS.sendLang(player, "teleport.in", Map.of("%seconds%", String.valueOf(time - spent)));
                 }
             });
         }, 2, 2);
     }
 
     public static void addToQueue(Player player, Warp warp) {
-        queue.put(player, new Pair<>(warp, System.currentTimeMillis()));
+        queue.put(player, new TeleportData(warp, System.currentTimeMillis(), warp.getLocation(), warp.getCurrency(), warp.getTeleportPrice()));
     }
 
-    public static Map<Player, Pair<Warp, Long>> getQueue() {
+    public static Map<Player, TeleportData> getQueue() {
         return queue;
     }
 }
